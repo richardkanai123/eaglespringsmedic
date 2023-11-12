b1784f5039530f970916f6c939a72b9783dd611f
Richard Kanai(李凯男) <richardkanainjeri@gmail.com>
2023-11-12 13:17:37 +0300

    Pending Mark Messaga as read

--- components/Dashboard/MessagesHolder.js
+++ components/Dashboard/MessagesHolder.js
@@ -12,7 +12,7 @@ import { format } from 'date-fns'
 import { useAuthState } from "react-firebase-hooks/auth"
 import Login from "@/components/Dashboard/Login"
 import { Button } from "@/components/ui/button"
-import { deleteDoc } from "firebase/firestore"
+import { deleteDoc, doc, setDoc } from "firebase/firestore"
 import { toast } from "react-toastify"
 import { useRouter } from "next/navigation"
 
@@ -37,12 +37,16 @@ const MessagesHolder = ({ messages }) => {
                     {
                         messages.map((msg) => (
                             <AccordionItem key={msg.id} value={msg.id}>
-                                <AccordionTrigger className={cn('w-full text-lg font-semibold')}>Sent by: {msg.name || msg.Name} on {new Date((msg.time.seconds * 1000) + ((msg.time.nanoseconds) / 1000000000)).toLocaleDateString()} at {new Date((msg.time.seconds * 1000) + ((msg.time.nanoseconds) / 1000000000)).toLocaleTimeString()}
 
-                                    <span>Msg status </span>
+                                <AccordionTrigger className={cn('w-full px-4 text-lg font-semibold')}>
+                                    {
+                                        msg.status === 'read' ? <span className="p-1 bg-green-600 h-5 w-5 rounded-full"></span> : <span className="p-1 bg-yellow-600 h-5 w-5 rounded-full"></span>
+                                    }
+                                    <p>    Sent by: {msg.name || msg.Name} on {new Date((msg.time.seconds * 1000) + ((msg.time.nanoseconds) / 1000000000)).toLocaleDateString()} at {new Date((msg.time.seconds * 1000) + ((msg.time.nanoseconds) / 1000000000)).toLocaleTimeString()}</p>
+
                                 </AccordionTrigger>
-                                <AccordionContent className={cn('bg-sky-200 dark:bg-slate-800 p-1 rounded ')}>
-                                    <p className="text-base font-semibold mb-1">{msg.Message || msg.message}</p>
+                                <AccordionContent className={cn('px-3 py-1 bg-sky-200 dark:bg-slate-800  rounded ')}>
+                                    <p className="  text-base font-semibold mb-1">{msg.Message || msg.message}</p>
                                     <p className="flex text-base font-medium flex-col">
                                         Contact: <span>{msg.email || ''}</span> <span> {msg.phoneNumber || ''}</span>
                                     </p>
@@ -50,7 +54,16 @@ const MessagesHolder = ({ messages }) => {
                                         {
                                             msg.status === "unread" &&
                                             <Button
-
+                                                onClick={async () => {
+                                                    try {
+                                                        await setDoc(doc(db, 'messages', msg.id), { status: 'read' }, { merge: true })
+                                                            .then(() => toast.info('Message deleted!'))
+                                                            .then(() => Router.refresh())
+                                                    } catch (error) {
+                                                        console.log(error)
+                                                        toast.error(error.message)
+                                                    }
+                                                }}
                                                 className={cn('bg-yellow-700')}>
                                                 Mark as read
                                             </Button>
@@ -60,10 +73,11 @@ const MessagesHolder = ({ messages }) => {
                                             <Button
                                                 onClick={async () => {
                                                     try {
-                                                        await deleteDoc(messagesCollection, msg.id)
+                                                        await deleteDoc(doc(db, 'messages', msg.id))
                                                             .then(() => toast.info('Message deleted!'))
                                                             .then(() => Router.refresh())
                                                     } catch (error) {
+                                                        console.log(error)
                                                         toast.error(error.message)
                                                     }
                                                 }}
