04abb6a3472e6dce9ed4f416f6968d59a19ba27a
Richard Kanai(李凯男) <richardkanainjeri@gmail.com>
2023-11-20 15:36:59 +0300

    dynamic + revalidation

--- app/Dashboard/Messages/page.js
+++ app/Dashboard/Messages/page.js
@@ -1,9 +1,13 @@
-import MessagesHolder from '@/components/Dashboard/MessagesHolder'
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
+import MessagesHolder from '@/components/Dashboard/MessagesHolder'
 const DashBoardMessages = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASEURL}/api/messages`, {
-        cache: 'no-store'
-    })
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASEURL}/api/messages`,
+        {
+            cache: 'no-cache',
+
+        })
 
     if (!res.ok) {
         throw new Error("fetch error occured")
